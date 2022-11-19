92bf571a87af33debd723ef8d07cff8a3e810335
Dluter <tom230023@gmail.com>
2022-11-19 15:23:27 +0800

    test

--- 007_Queue(FIFO)/Queue_List2.java
+++ 007_Queue(FIFO)/Queue_List2.java
@@ -1,5 +1,51 @@
 public class Queue_List2 {
-  public static void main(String[] args) {
-    System.out.println("f");
+  static class Node {
+    Integer val;
+    Node next;
+
+    public Node(Integer val) {
+      this.val = val;
+    }
+
+    Integer[] nums;
+    Node node_front = null;
+    Node node_end = null;
+
+    public void Queue_List2(Integer[] nums) {
+      this.nums = nums;
+    }
+
+    public void build_queue() {
+      for (int i = 0; i < nums.length; i++) {
+        offer(nums[i]);
+      }
+    }
+
+    public void offer(Integer val) {
+      if (node_front == null) {
+        node_front = new Node(val);
+        node_end = node_front;
+      } else {
+        Node node_new = new Node(val);
+        node_end.next = node_new;
+        node_end = node_new;
+      }
+    }
+
+    public Integer poll() {
+      if (node_front == null) {
+        return null;
+      }
+
+      Node node = node_front;
+      node_front = node_front.next;
+
+      if (node_front == null) {
+        node_end = null;
+      }
+
+      return node.val;
+    }
+
   }
 }
---
+++ 007_Queue(FIFO)/Queue_List2.md
@@ -0,0 +1,38 @@
+#### offer
+```java
+public void offer(Integer val) {
+  if (node_front == null) {
+    node_front = new Node(val);
+    node_end = node_front;
+  } else {
+    Node node_new = new Node(val);
+    node_end.next = node_new;
+    node_end = node_new;
+  }
+}
+```
+
+1. 新增的時候如果讀頭是null代表讀頭以及寫頭需要初始化
+2. 反之新增元素給寫頭，並把寫頭 = 自己
+
+---
+
+#### offer
+```java
+public Integer poll() {
+  if (node_front == null) {
+    return null;
+  }
+
+  Node node = node_front;
+  node_front = node_front.next;
+
+  if (node_front == null) {
+    node_end = null;
+  }
+
+  return node.val;
+}
+```
+
+1. 讀頭 = 下一個元素
